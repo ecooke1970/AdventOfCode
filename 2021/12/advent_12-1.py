b69dbe9025fe24dfdafb6d749d6d93d3b1c2fae5
Erik <46272257+ecooke1970@users.noreply.github.com>
2022-02-14 16:17:01 -0500

    Started AOC 2021 12-1

---
+++ 2021/12/advent_12-1.py
@@ -0,0 +1,25 @@
+# Erik Cooke
+# Advent of Code 2021 12 part 1
+
+
+def print_grid():
+    for row in grid:
+        print(row)
+    print()
+
+
+if __name__ == "__main__":
+    # Open input file
+    with open('code_input12.txt') as file:
+        for line in file:
+            x = line.strip().split("-")
+            print(x)
+
+
+
+
+
+
+
+
+
