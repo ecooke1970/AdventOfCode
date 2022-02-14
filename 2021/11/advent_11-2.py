d82d0e41561d2e24a2684f9f4816fb773cd39e89
Erik <46272257+ecooke1970@users.noreply.github.com>
2022-02-14 01:11:05 -0500

    Finish AOC 2021 11-1 and 11-2

--- 2021/11/advent_11-2.py
+++ 2021/11/advent_11-2.py
@@ -1,7 +1,7 @@
 # Erik Cooke
-# Advent of Code 2021 11 part 1
+# Advent of Code 2021 11 part 2
 
-STEPS = 100
+STEPS = 500
 grid = []
 flash_count = 0
 offset_x = [-1,  0,  1, 1, 1, 0, -1, -1]
@@ -25,10 +25,12 @@ def process_flashes():
     global flash_count
     count = 1
     while count > 0:
+        flash_count = 0
         count = 0
         for y in range(len(grid)):
             for x in range(len(grid[0])):
                 if grid[y][x] == 0:
+                    flash_count += 1
                     continue
                 elif grid[y][x] > 9:
                     for (i, j) in zip(offset_x, offset_y):
@@ -39,7 +41,6 @@ def process_flashes():
                         elif grid[ny][nx] > 0:
                             grid[ny][nx] += 1
                     grid[y][x] = 0
-                    flash_count += 1
                     count += 1
     print_grid()
 
@@ -53,11 +54,14 @@ if __name__ == "__main__":
             grid.append(x)
 
     print_grid()
-    for step in range(STEPS):
+    for step in range(1, STEPS + 1):
         # Advance each octopus by 1
         process_step()
         # process grid
         process_flashes()
+        if flash_count == len(grid[0]) * len(grid):
+            print("Step# = ", step)
+            break
 
     print(flash_count)
 
