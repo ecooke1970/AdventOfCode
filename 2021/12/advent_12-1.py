0872097abc4f7a7d681c838f15a0a88e748622a9
Erik <46272257+ecooke1970@users.noreply.github.com>
2022-02-17 23:40:34 -0500

    Update advent_12-1.py

--- 2021/12/advent_12-1.py
+++ 2021/12/advent_12-1.py
@@ -1,12 +1,20 @@
 # Erik Cooke
 # Advent of Code 2021 12 part 1
 grid = []
+caves = {}
 
 
 def print_grid():
     for row in grid:
         print(row)
     print()
+
+
+def create_graph():
+    for cave in grid:
+        caves[cave[0]] = [cave[1]]
+
+    print(caves)
 
 
 if __name__ == "__main__":
@@ -17,6 +25,7 @@ if __name__ == "__main__":
             grid.append(x)
 
     print_grid()
+    create_graph()
 
 
 
