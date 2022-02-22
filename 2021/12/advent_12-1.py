fc68ad4dab86c71619e5b85d04581468689de245
Erik <46272257+ecooke1970@users.noreply.github.com>
2022-02-21 20:16:47 -0500

    create_graph working

--- 2021/12/advent_12-1.py
+++ 2021/12/advent_12-1.py
@@ -12,24 +12,22 @@ def print_grid():
 
 def create_graph():
     for cave in grid:
-        print(cave[0], " ", cave[1])
-        if cave[0] in caves:
-            if cave[0] == 'end':
-                break
-            else:
-                caves[cave[0]].append(cave[1])
+        print("Part 1 ", cave[0], " ", cave[1])
+        if cave[0] == 'end' or cave[1] == 'start':
+            continue
+        elif cave[0] in caves:
+            caves[cave[0]].append(cave[1])
         else:
             caves[cave[0]] = []
             caves[cave[0]].append(cave[1])
 
         print(caves)
 
-        print(cave[0], " ", cave[1])
-        if cave[1] in caves:
-            if cave[1] == 'end' or cave[0] == 'start':
-                break
-            else:
-                caves[cave[1]].append(cave[0])
+        print("Part 2 ", cave[0], " ", cave[1])
+        if cave[1] == 'end' or cave[0] == 'start':
+            continue
+        elif cave[1] in caves:
+            caves[cave[1]].append(cave[0])
         else:
             caves[cave[1]] = []
             caves[cave[1]].append(cave[0])
@@ -45,7 +43,7 @@ if __name__ == "__main__":
             x = line.strip().split("-")
             grid.append(x)
 
-    print_grid()
+    # print_grid()
     create_graph()
 
 
