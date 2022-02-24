40d74eacab85b14d71f369cd86fbf797a4083791
Erik <46272257+ecooke1970@users.noreply.github.com>
2022-02-23 23:14:53 -0500

    Update advent_12-1.py

--- 2021/12/advent_12-1.py
+++ 2021/12/advent_12-1.py
@@ -4,20 +4,19 @@ grid = []
 caves = {}
 
 
-def print_grid():
-    for row in grid:
+def print_list(a):
+    for row in a:
         print(row)
     print()
 
 
 def print_caves():
     for i, value in caves.items():
-        print(i, " ", value)
+        print(f'{i:>5} - {value}')
 
 
 def create_graph():
     for cave in grid:
-        # print("Part 1 ", cave[0], " ", cave[1])
         if cave[0] == 'end' or cave[1] == 'start':
             continue
         elif cave[0] in caves:
@@ -35,23 +34,60 @@ def create_graph():
             caves[cave[1]] = []
             caves[cave[1]].append(cave[0])
 
+    # def find_all_paths(graph, start, end, path=[]):
+    #     path = path + [start]
+    #     if start == end:
+    #         return [path]
+    #     if not graph.has_key(start):
+    #         return []
+    #     paths = []
+    #     for node in graph[start]:
+    #         if node not in path:
+    #             newpaths = find_all_paths(graph, node, end, path)
+    #             for newpath in newpaths:
+    #                 paths.append(newpath)
+    #     return paths
 
-if __name__ == "__main__":
+
+def find_paths(s, e, path=[], dupe_small_cave=False):
+    # global caves
+    path = path + [s]
+    if s == e:
+        return [path]
+    if s not in caves:
+        return []
+    paths = []
+    for node in caves[s]:
+        if node.islower() and node in path and dupe_small_cave is False:
+            dupe_small_cave = True
+            new_paths = find_paths(node, e, path, dupe_small_cave)
+            for new_path in new_paths:
+                paths.append(new_path)
+        elif node.isupper() or node not in path:
+            new_paths = find_paths(node, e, path, dupe_small_cave)
+            for new_path in new_paths:
+                paths.append(new_path)
+    return paths
+
+
+def main():
     # Open input file
     with open('code_input12.txt') as file:
         for line in file:
             x = line.strip().split("-")
+            if x[1] == 'start' or x[0] == 'end':
+                x[0], x[1] = x[1], x[0]
             grid.append(x)
 
-    # print_grid()
+    print_list(grid)
+    print()
     create_graph()
     print_caves()
-    # find_paths()
+    print()
+    all_paths = find_paths('start', 'end')
+    print_list(all_paths)
+    print("Number of paths = ", len(all_paths))
 
 
-
-
-
-
-
-
+if __name__ == "__main__":
+    main()
