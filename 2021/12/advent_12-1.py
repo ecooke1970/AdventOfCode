8186cb09b64e63e9c1990b9c151c5137b7885541
Erik <46272257+ecooke1970@users.noreply.github.com>
2022-03-08 09:25:10 -0500

    Update advent_12-1.py

--- 2021/12/advent_12-1.py
+++ 2021/12/advent_12-1.py
@@ -10,8 +10,6 @@ def print_list(a):
     print()
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 def print_caves():
     for i, value in caves.items():
         print(f'{i:>5} - {value}')
@@ -19,16 +17,7 @@ def print_caves():
 
 def create_graph():
     for cave in grid:
-=======
-def create_graph():
-    for cave in grid:
         print("Part 1 ", cave[0], " ", cave[1])
->>>>>>> parent of bdb6fbb (Update advent_12-1.py)
-=======
-def create_graph():
-    for cave in grid:
-        print("Part 1 ", cave[0], " ", cave[1])
->>>>>>> parent of bdb6fbb (Update advent_12-1.py)
         if cave[0] == 'end' or cave[1] == 'start':
             continue
         elif cave[0] in caves:
@@ -48,54 +37,47 @@ def create_graph():
             caves[cave[1]] = []
             caves[cave[1]].append(cave[0])
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-    # def find_all_paths(graph, start, end, path=[]):
-    #     path = path + [start]
-    #     if start == end:
-    #         return [path]
-    #     if not graph.has_key(start):
-    #         return []
-    #     paths = []
-    #     for node in graph[start]:
-    #         if node not in path:
-    #             newpaths = find_all_paths(graph, node, end, path)
-    #             for newpath in newpaths:
-    #                 paths.append(newpath)
-    #     return paths
 
-
-def find_paths(s, e, path=[], dupe_small_cave=False):
-    # global caves
-    path = path + [s]
-    if s == e:
+def find_all_paths(start, end, path=[]):
+    path = path + [start]
+    if start == end:
         return [path]
-    if s not in caves:
+    if start not in caves:
         return []
     paths = []
-    for node in caves[s]:
-        if node.islower() and node in path and dupe_small_cave is False:
-            dupe_small_cave = True
-            new_paths = find_paths(node, e, path, dupe_small_cave)
-            for new_path in new_paths:
-                paths.append(new_path)
-        elif node.isupper() or node not in path:
-            new_paths = find_paths(node, e, path, dupe_small_cave)
-            for new_path in new_paths:
-                paths.append(new_path)
+    for node in caves[start]:
+        if node not in path:
+            newpaths = find_all_paths(node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
     return paths
 
 
+# def find_paths(s, e, path=[], dupe_small_cave=False):
+#     # global caves
+#     path = path + [s]
+#     if s == e:
+#         return [path]
+#     if s not in caves:
+#         return []
+#     paths = []
+#     for node in caves[s]:
+#         if node.islower() and node in path and dupe_small_cave is False:
+#             dupe_small_cave = True
+#             new_paths = find_paths(node, e, path, dupe_small_cave)
+#             for new_path in new_paths:
+#                 paths.append(new_path)
+#         elif node.isupper() or node not in path:
+#             new_paths = find_paths(node, e, path, dupe_small_cave)
+#             for new_path in new_paths:
+#                 paths.append(new_path)
+#     return paths
+
+
 def main():
-=======
-=======
->>>>>>> parent of bdb6fbb (Update advent_12-1.py)
-        print(caves)
-        print()
+    print(caves)
+    print()
 
-
-if __name__ == "__main__":
->>>>>>> parent of bdb6fbb (Update advent_12-1.py)
     # Open input file
     with open('code_input12.txt') as file:
         for line in file:
@@ -107,23 +89,11 @@ if __name__ == "__main__":
     print_list(grid)
     print()
     create_graph()
-<<<<<<< HEAD
-<<<<<<< HEAD
     print_caves()
     print()
-    all_paths = find_paths('start', 'end')
+    all_paths = find_all_paths('start', 'end')
     print_list(all_paths)
     print("Number of paths = ", len(all_paths))
-=======
-=======
->>>>>>> parent of bdb6fbb (Update advent_12-1.py)
-
-
-
-
-
-
->>>>>>> parent of bdb6fbb (Update advent_12-1.py)
 
 
 if __name__ == "__main__":
