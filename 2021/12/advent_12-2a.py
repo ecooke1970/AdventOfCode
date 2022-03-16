54f6a78dd6a75390aeeb7445b692f5bf30b3703b
Erik <46272257+ecooke1970@users.noreply.github.com>
2022-03-16 09:23:23 -0400

    Update advent_12-2a.py

--- 2021/12/advent_12-2a.py
+++ 2021/12/advent_12-2a.py
@@ -17,6 +17,29 @@ def print_caves():
         print(f'{i:>5} - {value}')
 
 
+def find_paths(start, end, can_twice, path=[]):
+    path = path + [start]
+    if start == end:
+        return [path]
+    if start not in caves:
+        return []
+    paths = []
+    for node in caves[start]:
+        if node.islower():
+            if start not in path:
+                new_paths = find_paths(node, end, can_twice, path)
+                for new_path in new_paths:
+                    paths.append(new_path)
+            elif can_twice and node not in {'start', 'end'}:
+                new_paths = find_paths(node, end, False, path)
+                for new_path in new_paths:
+                    paths.append(new_path)
+        else:
+            new_paths = find_paths(node, end, can_twice, path)
+            for new_path in new_paths:
+                paths.append(new_path)
+
+
 # def find_paths(s, e, path=[]):
 #     # global caves
 #     path = path + [s]
@@ -46,6 +69,10 @@ def main():
             caves[b].append(a)
 
     print_caves()
+
+    all_paths = find_paths('start', 'end', False)
+
+    print(len(all_paths))
 
     # print_list(grid)
     # print()
