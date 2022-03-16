a7e0d1ef3bc6214efe4f45d17d943144538721af
Erik <46272257+ecooke1970@users.noreply.github.com>
2022-03-16 19:32:37 -0400

    Finished AOC 2021 13-2

--- 2021/13/advent_13-1.py
+++ 2021/13/advent_13-1.py
@@ -4,42 +4,52 @@ from collections import defaultdict
 
 coordinates = []
 flip = []
+grid = []
 
 
 def print_list(a):
     for row in a:
-        print(row)
+        for item in row:
+            if item == 1:
+                print("#", end="")
+            else:
+                print(" ", end="")
+        print()
     print()
 
 
-def flip_page(flip_list, grid):
-    print(flip_list)
+def flip_page(flip_list):
+    global grid
+    # flip_count = 0
     for i, j in flip_list:
         if i == 'y':
-            print("Flip Y", j)
+            # print("Flip Y", j)
             for y in range(len(grid)):
                 for x in range(len(grid[0])):
                     if grid[y][x] == 1 and y > j:
-                        grid[y][x] = 0
                         diff = j - (y - j)
                         grid[diff][x] = 1
-                        print(diff)
-        else:
-            print("Flip X", j)
+            grid = grid[:-j]
+        elif i == 'x':
+            # print("Flip X", j)
             for y in range(len(grid)):
                 for x in range(len(grid[0])):
                     if grid[y][x] == 1 and x > j:
-                        grid[y][x] = 0
                         diff = j - (x - j)
                         grid[y][diff] = 1
+            for k, row in enumerate(grid):
+                grid[k] = row[:j]
+        # flip_count += 1
+        # if flip_count >= 1:
+        #     break
 
 
-def fill_grid(grid):
+def fill_grid():
     for x, y in coordinates:
         grid[y][x] = 1
 
 
-def count_grid(grid):
+def count_grid():
     num = 0
     for row in grid:
         num += row.count(1)
@@ -47,6 +57,7 @@ def count_grid(grid):
 
 
 def main():
+    global grid
     # Open input file
     high_x, high_y = 0, 0
     with open('code_input13.txt') as file:
@@ -63,15 +74,13 @@ def main():
                 flip.append(i)
 
     grid = [[0 for i in range(high_x + 1)] for j in range(high_y + 1)]
-    # arr = [[0 for i in range(cols)] for j in range(rows)]
-    fill_grid(grid)
-    # print(high_x, high_y)
-    print_list(grid)
-    # print_list(coordinates)
-    flip_page(flip, grid)
-    print_list(grid)
 
-    print(count_grid(grid))
+    fill_grid()
+
+    flip_page(flip)
+
+    print_list(grid)
+    # print(count_grid())
 
 
 if __name__ == "__main__":
