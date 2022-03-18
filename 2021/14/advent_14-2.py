a8387ed6e945c1307aea2395477c59c6b46f25d9
Erik <46272257+ecooke1970@users.noreply.github.com>
2022-03-18 12:05:06 -0400

    working on aoc 2021 day 14-2

--- 2021/14/advent_14-2.py
+++ 2021/14/advent_14-2.py
@@ -1,36 +1,57 @@
 # Erik Cooke
 # Advent of Code 2021 14 part 2
 
-from collections import Counter
+from collections import Counter, defaultdict
 
-ITERATIONS = 40
+
+ITERATIONS = 10
 insert_rules = {}
-polymer = ""
+first_polymer = ""
+polymer = defaultdict(int)
+count_polymers = defaultdict(int)
 
 
 def polymer_pairs():
-    global polymer
-    new_polymer =
+    global first_polymer, count_polymers
+    for i in range(len(first_polymer) - 1):
+        polymer[first_polymer[i] + first_polymer[i+1]]
+        count_polymers[first_polymer[i]] += 1
+    count_polymers[first_polymer[-1]] += 1
+    # print(polymer)
+    # print("Count: ", count_polymers)
+
 
 def process_polymer():
-    global polymer
+    global polymer, count_polymers
     for i in range(ITERATIONS):
-        print(i, "BEGIN")
-        new_code = polymer[0]
-        for j in range(len(polymer) - 1):
-            code = polymer[j:j+2]
-            x = insert_rules[code]
-            new_code += x + code[1]
-        polymer = new_code
-        print(i, "END")
-        # print(polymer)
+        temp_polymer = defaultdict(int)
+        for item in polymer:
+            print("item:", item)
+            count_polymers[insert_rules[item]] += 1
+            print("count: ", count_polymers)
+
+            temp_polymer[item[0] + insert_rules[item]] += polymer[item] + 1
+            temp_polymer[insert_rules[item] + item[1]] += polymer[item] + 1
+        print("End")
+        polymer = temp_polymer
+        print("polymer:", polymer)
+    print(count_polymers)
+
+
+# def count_letters():
+#     global polymer
+#     count = defaultdict(int)
+#     for item in polymer:
+#         count[item[0]] += polymer.get(item)
+#         count[item[1]] += polymer.get(item)
+#     print(count)
 
 
 def main():
-    global polymer
+    global first_polymer
     # Open input file
     with open('code_input14.txt') as file:
-        polymer = file.readline().strip()
+        first_polymer = file.readline().strip()
         file.readline()
         for line in file:
             x = line.strip().split(" -> ")
@@ -38,11 +59,12 @@ def main():
 
     polymer_pairs()
     process_polymer()
-    z = Counter(polymer)
+    # count_letters()
+    # z = Counter(new_polymer)
     # print(z)
-    values = z.values()
+    # values = z.values()
     # print(values)
-    print(max(values) - min(values))
+    # print(max(values) - min(values))
 
 
 if __name__ == "__main__":
